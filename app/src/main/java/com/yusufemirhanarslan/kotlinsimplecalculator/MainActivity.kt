35db52202803ac3bf33c6c272e010f489ee31437
Yusuf Emirhan Arslan <arslanemirhan03.10@gmail.com>
2022-04-28 17:25:07 +0300

    Kotlin kullanarak temel seviye hesap makinesi

---
+++ app/src/main/java/com/yusufemirhanarslan/kotlinsimplecalculator/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.yusufemirhanarslan.kotlinsimplecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var result : Int? = null
+    var number1 : Int? = null
+    var number2 : Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+    fun mySum(view : View){
+
+        number1 = editText.text.toString().toIntOrNull()
+        number2 = editText2.text.toString().toIntOrNull()
+
+        if(number1 == null || number2 == null){
+            resultText.text = "Error!"
+        }else {
+             result = number1!! + number2!!
+            resultText.text = "Result: ${result}"
+        }
+
+
+    }
+
+    fun mySub(view : View){
+
+        number1 = editText.text.toString().toIntOrNull()
+        number2 = editText2.text.toString().toIntOrNull()
+
+        if(number1 == null || number2 == null){
+            resultText.text = "Error!"
+        }else {
+            result = number1!! - number2!!
+            resultText.text = "Result: ${result}"
+        }
+
+    }
+
+    fun myMultiply(view : View){
+
+        number1 = editText.text.toString().toIntOrNull()
+        number2 = editText2.text.toString().toIntOrNull()
+
+        if(number1 == null || number2 == null){
+            resultText.text = "Error!"
+        }else {
+            result = number1!! * number2!!
+            resultText.text = "Result: ${result}"
+        }
+
+    }
+
+    fun myDiv(view : View){
+
+        number1 = editText.text.toString().toIntOrNull()
+        number2 = editText2.text.toString().toIntOrNull()
+
+        if(number1 == null || number2 == null){
+            resultText.text = "Error!"
+        }else {
+            result = number1!! / number2!!
+            resultText.text = "Result: ${result}"
+        }
+
+
+    }
+}
